ae50c0cdf3e431b3f8f93276e4d768ba45d026cc
Chriisbrown <41648127+Chriisbrown@users.noreply.github.com>
2020-08-07 19:36:31 +0100

    Update L1AnalysisTrackMET.cc

--- L1TNtuples/src/L1AnalysisTrackMET.cc
+++ L1TNtuples/src/L1AnalysisTrackMET.cc
@@ -15,13 +15,13 @@ void L1Analysis::L1AnalysisTrackMET::SetTrackMET(const edm::Handle<l1t::TkEtMiss
 
   for (unsigned int i=0; i<TkEtMiss->size(); i++){
     if (name == "Track")
-      l1extra_.TksumEt = (TkEtMiss->at(i).etMiss());
+      l1extra_.TrkMet = (TkEtMiss->at(i).etMiss());
     if (name == "Cut")
-      l1extra_.CutsumEt = (TkEtMiss->at(i).etMiss());
+      l1extra_.CutTrkMet = (TkEtMiss->at(i).etMiss());
     if (name == "MVA")
-      l1extra_.MVAsumEt = (TkEtMiss->at(i).etMiss());
+      l1extra_.MVATrkMet = (TkEtMiss->at(i).etMiss());
     if (name == "Gen")
-      l1extra_.GensumEt = (TkEtMiss->at(i).etMiss());
+      l1extra_.GenMet = (TkEtMiss->at(i).etMiss());
       
 }
 }
